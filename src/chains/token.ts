9c015073501a0ee17f76523e5776d1af7b8c4543
Shamil Khalilov <shamfama@gmail.com>
2020-11-06 18:10:17 +0300

    Add NSBT

--- src/chains/token.ts
+++ src/chains/token.ts
@@ -1,4 +1,10 @@
+import { assetBalance } from '@waves/waves-transactions/dist/nodeInteraction'
 import { AvailableChains, Chain } from './chain'
+
+interface ChainBridgeConfig {
+  sourcePort: string
+  destinationPort: string
+}
 
 export type Token = {
   ticker: string
@@ -8,6 +14,7 @@ export type Token = {
   decimals: number
   assetId: string
   ERC20?: string
+  bridgeConfig?: ChainBridgeConfig
 }
 
 export const AvailableTokens: Record<string, Token> = {
@@ -36,6 +43,10 @@ export const AvailableTokens: Record<string, Token> = {
     assetId: '9sQutD5HnRvjM1uui5cVC4w9xkMPAfYEV8ymug3Mon2Y',
     ERC20: '0x29499dD7da98588077806a9Fd45048692b443A3F',
     decimals: 8,
+    bridgeConfig: {
+      sourcePort: '3PEDESe94yGyY8X3Gez4u7cyZCa4JTAT6h3',
+      destinationPort: '0x9922Ec4054571711ab2bBAcda9B9E40321260ACa'
+    }
   },
   SusyStagenet: {
     ticker: 'SIGN',
@@ -53,6 +64,19 @@ export const AvailableTokens: Record<string, Token> = {
     assetId: 'Ap4heStRGQbHAxR6qb9UFtJ3kBiGuumdnsd9JzTHwTTL',
     ERC20: '0x8478f3C11F167da1C7C2c57F5FB3a34B584A4217',
     decimals: 6,
+  },
+  NSBTMainnet: {
+    ticker: 'NSBT',
+    label: 'NSBT',
+    bg: 'black',
+    icon: '/img/icons/tokens/nsbt.svg',
+    assetId: '6nSpVyNH7yM69eg446wrQR94ipbbcmZMU1ENPwanC97g',
+    ERC20: '0x248B4a000ED2e5Acb3e04Fc836FF368d8e257eEd',
+    decimals: 6,
+    bridgeConfig: {
+      sourcePort: '3PMMhFMzC86MstkNopJMNJoyUhDVCBxADCe',
+      destinationPort: '0x4B559A1c1Bc2701AAD4267Fc7A86d117e02f06c4'
+    }
   },
 }
 
@@ -74,8 +98,9 @@ export function formLinkForChain(chain: Chain, address: string): string {
 
 export function getAvailableTokens(): Token[] {
   return [
-    // AvailableTokens.SignStagenet, 
+    // AvailableTokens.SignStagenet,
     // AvailableTokens.WBNBStagenet
-    AvailableTokens.SignMainnet
+    AvailableTokens.SignMainnet,
+    AvailableTokens.NSBTMainnet,
   ]
 }
--- src/services/misc/config.ts
+++ src/services/misc/config.ts
@@ -1,26 +1,26 @@
 
 
-interface ChainConfig {
-    luport?: string;
-    ibport?: string;
-}
+// interface ChainConfig {
+//     luport?: string;
+//     ibport?: string;
+// }
 
-interface EnvironmentConfig {
-    wavesChain?: ChainConfig;
-    ethereumChain?: ChainConfig;
-}
+// interface EnvironmentConfig {
+//     wavesChain?: ChainConfig;
+//     ethereumChain?: ChainConfig;
+// }
 
 
-export function processConfig(): EnvironmentConfig {
-    return {
-        wavesChain: {
-            // luport: process.env.WAVES_LU_PORT,
-            luport: '3PEDESe94yGyY8X3Gez4u7cyZCa4JTAT6h3',
-            ibport: process.env.WAVES_IB_PORT
-        },
-        ethereumChain: {
-            // luport: process.env.ETH_LU_PORT,
-            ibport: '0x9922Ec4054571711ab2bBAcda9B9E40321260ACa'
-        },
-    }
-}
+// export function processConfig(): EnvironmentConfig {
+//     return {
+//         wavesChain: {
+//             // luport: process.env.WAVES_LU_PORT,
+//             luport: '3PEDESe94yGyY8X3Gez4u7cyZCa4JTAT6h3',
+//             ibport: process.env.WAVES_IB_PORT
+//         },
+//         ethereumChain: {
+//             // luport: process.env.ETH_LU_PORT,
+//             ibport: '0x9922Ec4054571711ab2bBAcda9B9E40321260ACa'
+//         },
+//     }
+// }
