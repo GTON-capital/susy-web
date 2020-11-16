dd463a0bbe686615baea17be80cd3718fa3c4042
Shamil Khalilov <shamfama@gmail.com>
2020-11-16 13:11:30 +0300

    Add redirect for demo

---
+++ src/pages/explorer.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>1</div>
+</template>
+
+<script>
+  export default {
+    middleware({ store, redirect }) {
+      return redirect('https://explorer.gravity.tech/nodes')
+    }
+  }
+</script>
