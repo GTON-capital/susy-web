c1965a6b357c65973d86992b5e6f06350cec9b37
Shamil Khalilov <shamfama@gmail.com>
2021-04-09 00:24:56 +0300

    Fix ga

--- nuxt.config.js
+++ nuxt.config.js
@@ -62,7 +62,10 @@ export default {
    ** Nuxt.js dev-modules
    */
   buildModules: [
-    ['@nuxt/typescript-build', { typeCheck: false }]
+    ['@nuxt/typescript-build', { typeCheck: false }],
+	['@nuxtjs/google-analytics', {
+      id: 'UA-178874521-1'
+    }]
   ],
   /*
    ** Nuxt.js modules
